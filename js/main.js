d479f2dba590f0b856b193f42d7b9d5602cc3709
Rodrigo Ferreira <rodrigo_ferreira89@outlook.com>
2023-09-15 18:00:57 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const btnMenu = document.querySelector('.btn-menu-header');
+const nav = document.querySelector('#ul-menu');
+const dropdownMobile = document.querySelector('.menu-dropdow')
+
+btnMenu.addEventListener('click', ()=> {
+    nav.classList.toggle('ativar-menu');
+});
+
+dropdownMobile.addEventListener('click', ()=> {
+    
+})
